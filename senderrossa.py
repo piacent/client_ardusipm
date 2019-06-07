a636cb0b67ac37c13668d69f2cda0d25d814aee5
piacent <stefano.piacentini@hotmail.com>
2019-06-07 11:35:34 +0200

    Small improvements

--- senderrossa.py
+++ senderrossa.py
@@ -76,6 +76,7 @@ client.loop_start()
 time.sleep(1)
 #Check message
 client.publish("daqardusipm/pyclient_task", "start_daq ARDU_ROSSA")
+time.sleep(0.5)
 
 file_path = "rossa.TXT"
 l=file_len(file_path)
@@ -90,6 +91,7 @@ with open(file_path, 'r') as logfile:
 		if n_prev!=n_cont:
 			print(str(n_cont*10)+' % sent.')
 		if i_cont==l-1:
+			time.sleep(0.5)
 			client.publish("daqardusipm/pyclient_task", "end_daq ARDU_ROSSA")
 			print("DAQ ended.")
 		time.sleep(0.01)
